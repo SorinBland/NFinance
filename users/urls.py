7e53541b004f90b1921a12fdcd57a0bb154fb78f
Sorin Bland <sorin.bland@yahoo.com>
2021-04-21 19:54:13 +0300

    added email activation + pass recovery

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from users.views import register
+from users.views import register, activate
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('register/', register, name='register'),
+    path('activate/<uidb64>/<token>/', activate, name='activate'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,22 +1,57 @@
-from django.contrib.auth import login
-from django.shortcuts import render, redirect
-from django.urls import reverse
-
+from django.contrib.auth import login, get_user_model
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from users.forms import CustomUserCreationForm
+
+UserModel = get_user_model()
 
 
 def register(request):
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return render(
-                request, "login.html",
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = "Activate your account."
+            message = render_to_string('accounts/acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                mail_subject, message, to=[to_email]
             )
+            email.send()
+            login(request, user)
+            return HttpResponse('Please confirm your email address to complete the registration.')
     else:
         form = CustomUserCreationForm()
     args = {'form': form}
     return render(
-        request, "registration/users.html", args
+        request, "registration/signup.html", args
     )
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = UserModel._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
