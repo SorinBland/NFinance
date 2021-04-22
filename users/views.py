ee0d8f0296fae5f021b9b1b731e78637c8acef15
Sorin Bland <sorin.bland@yahoo.com>
2021-04-22 19:48:08 +0300

    added acc mail

--- users/views.py
+++ users/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import login, get_user_model
 from django.contrib.auth.models import User
-from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
 from django.http import HttpResponse
@@ -9,7 +8,7 @@ from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from users.forms import CustomUserCreationForm
-
+from .tokens import account_activation_token
 UserModel = get_user_model()
 
 
@@ -22,11 +21,11 @@ def register(request):
             user.save()
             current_site = get_current_site(request)
             mail_subject = "Activate your account."
-            message = render_to_string('accounts/acc_active_email.html', {
+            message = render_to_string('registration/acc_activate_email.html', {
                 'user': user,
                 'domain': current_site.domain,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': default_token_generator.make_token(user),
+                'token': account_activation_token.make_token(user),
             })
             to_email = form.cleaned_data.get('email')
             email = EmailMessage(
@@ -46,10 +45,10 @@ def register(request):
 def activate(request, uidb64, token):
     try:
         uid = urlsafe_base64_decode(uidb64).decode()
-        user = UserModel._default_manager.get(pk=uid)
+        user = User.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
-    if user is not None and default_token_generator.check_token(user, token):
+    if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
         return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
