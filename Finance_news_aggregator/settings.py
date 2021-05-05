1e68831608072566f11dc1bf12dd5f3ca2dd2eaa
Sorin Bland <sorin.bland@yahoo.com>
2021-05-05 19:06:14 +0300

    added hosts

--- Finance_news_aggregator/settings.py
+++ Finance_news_aggregator/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = SECRET_KEY_LOCAL
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["nfinance.pythonanywhere.com", "127.0.0.1"]
 
 # Application definition
 
