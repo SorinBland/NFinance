b98e6102210143bb0905003d01d51b4874b992e2
Sorin Bland <sorin.bland@yahoo.com>
2021-05-05 19:55:24 +0300

    gitignore update

--- finance_app/apps.py
+++ finance_app/apps.py
@@ -5,11 +5,11 @@ from django.apps import AppConfig
 class FinanceAppConfig(AppConfig):
     name = 'finance_app'
 
-    def ready(self):
-        from finance_app.scraper import scrape, scrape_wsj
-        from apscheduler.schedulers.background import BackgroundScheduler
-        print('Initializing scheduler')
-        scheduler = BackgroundScheduler()
-        scheduler.add_job(scrape, 'interval', minutes=10)
-        scheduler.add_job(scrape_wsj, 'interval', minutes=10)
-        scheduler.start()
+    # def ready(self):
+    #     from finance_app.scraper import scrape, scrape_wsj
+    #     from apscheduler.schedulers.background import BackgroundScheduler
+    #     print('Initializing scheduler')
+    #     scheduler = BackgroundScheduler()
+    #     scheduler.add_job(scrape, 'interval', minutes=10)
+    #     scheduler.add_job(scrape_wsj, 'interval', minutes=10)
+    #     scheduler.start()
